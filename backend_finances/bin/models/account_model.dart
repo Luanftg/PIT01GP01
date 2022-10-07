10282ba9803f376dcdf49330a03427e2319c55a5
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-07 02:06:38 -0300

    [feat]: create account model

---
+++ backend_finances/bin/models/account_model.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class AccountModel {
+  final int id;
+  final String name;
+  final String imagem;
+  double balance;
+  AccountModel({
+    required this.id,
+    required this.name,
+    required this.imagem,
+    required this.balance,
+  });
+}
