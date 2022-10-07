1ee529a3f3c65c23bae2a41a87bdee41fcab25da
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-07 02:00:33 -0300

    [feat]: create CRUD on accounts routes

--- backend_finances/bin/api/accounts_api.dart
+++ backend_finances/bin/api/accounts_api.dart
@@ -9,6 +9,21 @@ class AccountsApi {
       return Response.ok('Acessou a rota [account]');
     });
 
+    router.post('/accounts', (Request req) {
+      return Response.ok('Postou na rota [account]');
+    });
+
+    router.put('/accounts', (Request req) {
+      String? id = req.url.queryParameters['id'];
+
+      return Response.ok('Atualizou a rota [account]');
+    });
+
+    router.delete('/accounts', (Request req) {
+      String? id = req.url.queryParameters['id'];
+      return Response.ok('Deletou a rota [account]');
+    });
+
     return router;
   }
 }
