d8e82ecc9d16e06dc72eb94ab80d2f8ec9d1ec09
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-07 02:10:54 -0300

    [refact]: JSON serialization on account model

--- backend_finances/bin/models/account_model.dart
+++ backend_finances/bin/models/account_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class AccountModel {
   final int id;
@@ -10,4 +12,32 @@ class AccountModel {
     required this.imagem,
     required this.balance,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'imagem': imagem,
+      'balance': balance,
+    };
+  }
+
+  factory AccountModel.fromMap(Map<String, dynamic> map) {
+    return AccountModel(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      imagem: map['imagem'] as String,
+      balance: map['balance'] as double,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AccountModel.fromJson(String source) =>
+      AccountModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'AccountModel(id: $id, name: $name, imagem: $imagem, balance: $balance)';
+  }
 }
