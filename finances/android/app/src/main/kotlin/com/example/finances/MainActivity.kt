c2da18d4a8863cc45f74a77c1665498c09dddeef
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-10 16:22:51 -0300

    first commit

--- finances/android/app/src/main/kotlin/com/example/finances/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.finances
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- finances/lib/domain/usecases/cadastrar_usuario/cadastrar_usuario_use_case.dart
+++
@@ -1,5 +0,0 @@
-import 'package:finances/domain/entities/usuario_entity.dart';
-
-abstract class CadastrarUsuarioUseCase {
-  Future<bool> call(Usuario novoUsuario);
-}
