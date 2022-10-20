43fa635dd16f53ed6f32f643482c14492c93ddb5
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-20 01:15:14 -0300

    [feat]: create CustomBottomAppBar

--- finances_group/lib/src/view/widgets/custom_bottom_app_bar.dart
+++ finances_group/lib/src/view/widgets/custom_bottom_app_bar.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
 
+class CustomBottomAppBar extends StatelessWidget {
+  final FloatingActionButtonLocation fabLocation;
+  final NotchedShape? shape;
+  static final List<FloatingActionButtonLocation> centerLocations = [
+    FloatingActionButtonLocation.centerDocked,
+    FloatingActionButtonLocation.centerFloat,
+  ];
+  const CustomBottomAppBar({
+    super.key,
+    required this.fabLocation,
+    this.shape = const CircularNotchedRectangle(),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: shape,
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.home_filled),
+            tooltip: 'Abre o menu de navegação',
+          ),
+          if (centerLocations.contains(fabLocation)) const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+            tooltip: 'Busca',
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.monetization_on),
+            tooltip: 'Favoritos',
+          ),
+        ],
+      ),
+    );
+  }
+}
