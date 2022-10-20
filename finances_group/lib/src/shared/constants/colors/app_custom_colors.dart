545a6a9b05433dec3a79d83347ba8577792b0575
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-20 01:14:45 -0300

    [feat]: create CustomColors

---
+++ finances_group/lib/src/shared/constants/colors/app_custom_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppCustomColors {
+  AppCustomColors._();
+
+  static const Color secondary = Color.fromRGBO(63, 222, 174, 1);
+  static const Color primary = Color.fromARGB(255, 144, 213, 118);
+  static const Color black = Color.fromARGB(255, 7, 10, 9);
+  static const Color white = Color.fromARGB(255, 230, 255, 247);
+}
