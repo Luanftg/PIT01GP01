8e5e37f1237825779d89f9f378dec4aad5c4839a
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-26 20:29:56 -0300

    [feat]: add custom Linear chart

--- finances_group/lib/src/view/pages/home_page.dart
+++ finances_group/lib/src/view/pages/home_page.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
+
+import 'package:finances_group/src/view/widgets/charts/custom_linear_chart.dart';
 import 'package:finances_group/src/view/widgets/homepage/app_bar.dart';
 import 'package:finances_group/src/view/widgets/homepage/balance.dart';
 import 'package:finances_group/src/view/widgets/homepage/body_transactions.dart';
-import 'package:finances_group/src/view/widgets/homepage/my_cards.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 
@@ -21,13 +23,19 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var random = Random();
+    var weekData =
+        List<double>.generate(100, (index) => random.nextDouble() * 100.0);
     return Scaffold(
       appBar: appBar,
       body: ListView(
-        children: const [
-          BalanceSection(),
-          MyCards(),
-          Bodytest(),
+        children: [
+          const BalanceSection(),
+          const SizedBox(height: 100),
+          CustomLinearChart(weekData: weekData),
+          const SizedBox(height: 100),
+          const Bodytest(),
+          //const MyCards(),
         ],
       ),
       bottomNavigationBar: bottomAppBAr,
