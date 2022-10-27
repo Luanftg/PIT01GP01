68c99a459abe762b41bd50842a6620d7d6be9edc
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-27 13:03:03 -0300

    fix: headline align and update style

--- finances_group/lib/src/view/widgets/homepage/body_transactions.dart
+++ finances_group/lib/src/view/widgets/homepage/body_transactions.dart
@@ -5,50 +5,53 @@ class Bodytest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          const ListTile(
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 30),
-            leading: Text(
-              'Últimas Transações',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            trailing: Icon(Icons.arrow_circle_right_outlined, size: 36),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20, right: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Últimas Transações',
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              const Icon(
+                Icons.arrow_circle_right_outlined,
+                size: 36,
+              ),
+            ],
           ),
-          Card(
-            margin:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: ListTile(
-              leading: Image.asset('assets/income.png'),
-              trailing: const Text('R\$'),
-              title: const Text('Salário'),
-              subtitle: const Text('Data, Hora'),
-            ),
+        ),
+        Card(
+          margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          child: ListTile(
+            leading: Image.asset('assets/income.png'),
+            trailing: const Text('R\$'),
+            title: const Text('Salário'),
+            subtitle: const Text('Data, Hora'),
           ),
-          Card(
-            margin:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: ListTile(
-              leading: Image.asset('assets/expense.png'),
-              title: const Text('Produtos Eletrônicos'),
-              subtitle: const Text('Data, Hora'),
-              trailing: const Text('R\$'),
-            ),
+        ),
+        Card(
+          margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          child: ListTile(
+            leading: Image.asset('assets/expense.png'),
+            title: const Text('Produtos Eletrônicos'),
+            subtitle: const Text('Data, Hora'),
+            trailing: const Text('R\$'),
           ),
-          Card(
-            margin:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: ListTile(
-              leading: Image.asset('assets/income.png'),
-              trailing: const Text('R\$'),
-              title: const Text('Jogo do Bicho'),
-              subtitle: const Text('Data, Hora'),
-            ),
-          )
-        ],
-      ),
+        ),
+        Card(
+          margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          child: ListTile(
+            leading: Image.asset('assets/income.png'),
+            trailing: const Text('R\$'),
+            title: const Text('Jogo do Bicho'),
+            subtitle: const Text('Data, Hora'),
+          ),
+        )
+      ],
     );
   }
 }
