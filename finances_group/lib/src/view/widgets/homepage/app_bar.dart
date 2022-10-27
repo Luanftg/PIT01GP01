81ea12a485564aada03805bc107eb53ca4c36627
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-27 18:50:28 -0300

    [style]: change toolbar height

--- finances_group/lib/src/view/widgets/homepage/app_bar.dart
+++ finances_group/lib/src/view/widgets/homepage/app_bar.dart
@@ -1,16 +1,15 @@
 import 'package:finances_group/src/view/widgets/homepage/action_app_bar.dart';
 import 'package:finances_group/src/view/widgets/homepage/title_app_bar.dart';
 import 'package:flutter/material.dart';
-
 import 'custom_bottom_app_bar.dart';
 
 final appBar = AppBar(
-  elevation: 0.0,
+  elevation: 0,
   centerTitle: false,
-  //backgroundColor: FinanceColors.primary,
-  toolbarHeight: 80,
+  toolbarHeight: 75,
   title: const TitleAppBar(),
   actions: const [ActionAppBar()],
+  automaticallyImplyLeading: false,
 );
 
 const bottomAppBAr = CustomBottomAppBar(
