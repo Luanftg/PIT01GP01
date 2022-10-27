245833d12517e8fda68c460a466292389b25a584
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-27 18:46:42 -0300

    [style] update text theme

--- finances_group/lib/src/design/theme/text_theme.dart
+++ finances_group/lib/src/design/theme/text_theme.dart
@@ -1,42 +1,36 @@
 import 'package:finances_group/src/design/colors/app_custom_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomAppTextTheme {
   CustomAppTextTheme._();
 
-  static TextStyle headline1 = const TextStyle(
+  static TextStyle headline1 = TextStyle(
       color: AppCustomColors.secondary,
-      fontFamily: 'GoogleSans',
+      fontFamily: GoogleFonts.abrilFatface().fontFamily,
       fontSize: 31,
       fontWeight: FontWeight.bold);
 
   static TextStyle headline2 = const TextStyle(
       color: AppCustomColors.secondary,
-      fontFamily: 'GoogleSans',
       fontSize: 24,
-      fontWeight: FontWeight.bold);
+      );
 
   static TextStyle body = const TextStyle(
-      color: AppCustomColors.white,
-      fontFamily: 'GoogleSans',
+      color: AppCustomColors.foreGround,
       fontSize: 16,
       fontWeight: FontWeight.normal);
 
   static TextStyle button = const TextStyle(
-      color: AppCustomColors.dark,
-      fontFamily: 'GoogleSans',
-      fontSize: 14,
-      fontWeight: FontWeight.bold);
+      color: AppCustomColors.dark, fontSize: 14, fontWeight: FontWeight.bold);
 
   static TextStyle caption = const TextStyle(
       color: AppCustomColors.grayLight,
-      fontFamily: 'GoogleSans',
       fontSize: 12,
       fontWeight: FontWeight.normal);
 
-   static TextStyle smallCaption = const TextStyle(
+  static TextStyle smallCaption = const TextStyle(
       color: AppCustomColors.grayLight,
-      fontFamily: 'GoogleSans',
       fontSize: 10,
       fontWeight: FontWeight.normal);
 }
