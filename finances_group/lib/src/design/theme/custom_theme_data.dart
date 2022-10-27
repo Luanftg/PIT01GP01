e7570b61721dbdb3670d29fe4967f1e6bd0438bb
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-27 13:17:24 -0300

    [style]: change floatingActionButton color

--- finances_group/lib/src/design/theme/custom_theme_data.dart
+++ finances_group/lib/src/design/theme/custom_theme_data.dart
@@ -1,28 +1,32 @@
 import 'package:finances_group/src/design/colors/app_custom_colors.dart';
 import 'package:finances_group/src/design/theme/text_theme.dart';
+import 'package:finances_group/src/view/widgets/homepage/app_bar.dart';
 import 'package:flutter/material.dart';
 
 final ThemeData draculaTheme = ThemeData(
-  brightness: Brightness.dark,
-  colorScheme: const ColorScheme(
-      background: AppCustomColors.dark,
-      brightness: Brightness.dark,
-      error: AppCustomColors.danger,
-      onBackground: AppCustomColors.white,
-      onError: AppCustomColors.white,
-      onPrimary: AppCustomColors.dark,
-      onSecondary: AppCustomColors.white,
-      onSurface: AppCustomColors.white,
-      primary: AppCustomColors.primary,
-      secondary: AppCustomColors.secondary,
-      surface: AppCustomColors.dark),
-  scaffoldBackgroundColor: AppCustomColors.dark,
+    brightness: Brightness.dark,
+    colorScheme: const ColorScheme(
+        background: AppCustomColors.dark,
+        brightness: Brightness.dark,
+        error: AppCustomColors.danger,
+        onBackground: AppCustomColors.white,
+        onError: AppCustomColors.white,
+        onPrimary: AppCustomColors.dark,
+        onSecondary: AppCustomColors.white,
+        onSurface: AppCustomColors.white,
+        primary: AppCustomColors.primary,
+        secondary: AppCustomColors.secondary,
+        surface: AppCustomColors.dark),
 
-  // TEXT THEME
-  textTheme: TextTheme(
-    headline2: CustomAppTextTheme.headline2,
-  ),
-  /* primaryColorLight: AppCustomColors.primary,
+    // TEXT THEME
+    textTheme: TextTheme(
+      headline2: CustomAppTextTheme.headline2,
+    ),
+    scaffoldBackgroundColor: AppCustomColors.dark,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: AppCustomColors.primary,
+        foregroundColor: AppCustomColors.dark)
+    /* primaryColorLight: AppCustomColors.primary,
   secondaryHeaderColor: AppCustomColors.secondary,
   bottomAppBarColor: AppCustomColors.primary,
   appBarTheme: const AppBarTheme(
@@ -34,6 +38,6 @@ final ThemeData draculaTheme = ThemeData(
   textTheme: TextTheme(
     caption: CustomAppTextTheme.caption,
   ), */
-);
+    );
 
 ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
