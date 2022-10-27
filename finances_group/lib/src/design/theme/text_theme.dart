074f1c2c3795628861f2797fa2b4e39dd33d91ac
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-27 13:08:49 -0300

    [style]: update Text Theme

--- finances_group/lib/src/design/theme/text_theme.dart
+++ finances_group/lib/src/design/theme/text_theme.dart
@@ -1,20 +1,42 @@
+import 'package:finances_group/src/design/colors/app_custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppTextTheme {
   CustomAppTextTheme._();
 
-  static TextStyle caption =
-      const TextStyle(fontFamily: 'GoogleSans', fontSize: 20);
+  static TextStyle headline1 = const TextStyle(
+      color: AppCustomColors.secondary,
+      fontFamily: 'GoogleSans',
+      fontSize: 31,
+      fontWeight: FontWeight.bold);
 
-  static TextStyle headline3 = const TextStyle(
-      fontFamily: 'GoogleSans', fontSize: 18, fontWeight: FontWeight.normal);
+  static TextStyle headline2 = const TextStyle(
+      color: AppCustomColors.secondary,
+      fontFamily: 'GoogleSans',
+      fontSize: 24,
+      fontWeight: FontWeight.bold);
 
-  static TextStyle headline4 = const TextStyle(
-      fontFamily: 'GoogleSans', fontSize: 28, fontWeight: FontWeight.bold);
+  static TextStyle body = const TextStyle(
+      color: AppCustomColors.white,
+      fontFamily: 'GoogleSans',
+      fontSize: 16,
+      fontWeight: FontWeight.normal);
 
-  static TextStyle headline6 = const TextStyle(
-      fontFamily: 'GoogleSans', fontSize: 22, fontWeight: FontWeight.bold);
+  static TextStyle button = const TextStyle(
+      color: AppCustomColors.dark,
+      fontFamily: 'GoogleSans',
+      fontSize: 14,
+      fontWeight: FontWeight.bold);
 
-  static TextStyle label = const TextStyle(
-      fontFamily: 'GoogleSans', fontSize: 12, fontStyle: FontStyle.italic);
+  static TextStyle caption = const TextStyle(
+      color: AppCustomColors.grayLight,
+      fontFamily: 'GoogleSans',
+      fontSize: 12,
+      fontWeight: FontWeight.normal);
+
+   static TextStyle smallCaption = const TextStyle(
+      color: AppCustomColors.grayLight,
+      fontFamily: 'GoogleSans',
+      fontSize: 10,
+      fontWeight: FontWeight.normal);
 }
