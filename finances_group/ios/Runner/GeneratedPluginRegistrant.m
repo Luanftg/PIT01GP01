d6e72bf5c74d9fa5017f76b2203b008b13b7337f
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-27 20:08:28 -0300

    update config

--- finances_group/ios/Runner/GeneratedPluginRegistrant.m
+++ finances_group/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,12 +6,6 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<flutter_statusbarcolor_ns/FlutterStatusbarcolorPlugin.h>)
-#import <flutter_statusbarcolor_ns/FlutterStatusbarcolorPlugin.h>
-#else
-@import flutter_statusbarcolor_ns;
-#endif
-
 #if __has_include(<path_provider_ios/FLTPathProviderPlugin.h>)
 #import <path_provider_ios/FLTPathProviderPlugin.h>
 #else
@@ -21,7 +15,6 @@
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FlutterStatusbarcolorPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterStatusbarcolorPlugin"]];
   [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
 }
 
