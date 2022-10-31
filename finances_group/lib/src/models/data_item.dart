f7a32eaa34b7d4f6b6bb78ec2f7c41b2402feea0
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-10-31 00:32:02 -0300

    [feat]: add list of data in home

---
+++ finances_group/lib/src/models/data_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DataItem {
+  final double value;
+  final String label;
+  final Color color;
+  DataItem({
+    required this.value,
+    required this.label,
+    required this.color,
+  });
+}
