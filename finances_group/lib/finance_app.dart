9a1753353c05a77e3534c7fcf9ab69dc47db7b22
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-11-21 13:23:02 -0300

    [refact]: add rouites to MaterialApp

--- finances_group/lib/finance_app.dart
+++ finances_group/lib/finance_app.dart
@@ -1,6 +1,7 @@
 //import 'package:finances_group/src/design/theme/custom_theme_data.dart';
 import 'package:finances_group/src/view/design/theme/custom_theme_data.dart';
 import 'package:finances_group/src/view/pages/home_page.dart';
+import 'package:finances_group/src/view/pages/register_finantial_movement_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -18,7 +19,11 @@ class FinanceApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FinanceApp',
-      home: const HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const RegisterFinantialMovementPage(),
+        '/home': (context) => const HomePage(),
+      },
       theme: draculaTheme,
     );
   }
