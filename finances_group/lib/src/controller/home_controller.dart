6fcc02a2b51d64b1203b4008696ca54ed76ea91b
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-11-21 13:36:20 -0300

    [feat]: add HomeController

--- finances_group/lib/src/controller/home_controller.dart
+++ finances_group/lib/src/controller/home_controller.dart
@@ -1,12 +1,15 @@
-import 'package:finances_group/src/data/fake_db.dart';
-import 'package:finances_group/src/data/repositories/finantial_movement_repository_local_imp.dart';
+import 'dart:developer';
+
+import 'package:finances_group/src/data/repositories/repository.dart';
 import 'package:finances_group/src/models/data_item.dart';
 import 'package:finances_group/src/models/finantial_movement.dart';
+import 'package:flutter/material.dart';
 
 class HomeController {
-  final FinantialMovementRepositoryLocalImp  _finantialMovementRepositoryLocalImp;
+  final IRepository<FinantialMovement> _finantialMovementRepositoryLocalImp;
   HomeController(this._finantialMovementRepositoryLocalImp);
 
+  double maxValue = 0;
   Future<bool> create(FinantialMovement finantialMovement) async {
     var result =
         await _finantialMovementRepositoryLocalImp.create(finantialMovement);
@@ -18,8 +21,34 @@ class HomeController {
     return result;
   }
 
-  List<DataItem> getList() => dataset;
-  //List<DataItem> getList() => List.;
+  List<DataItem>? getList(BuildContext context) {
+    var args =
+        ModalRoute.of(context)!.settings.arguments as List<FinantialMovement>;
+    var fmList = args;
+    for (var index = 0; index < fmList.length; index++) {
+      maxValue += fmList[index].value;
+      log((fmList[index].value / maxValue).toString());
+    }
+    log('UserfmList: $fmList');
+    var dataset = List.generate(
+      fmList.length,
+      (index) => DataItem(
+          value: fmList[index].value / maxValue,
+          label: fmList[index].category.label,
+          color: fmList[index].category.color),
+    );
 
-  List<double> getWeekdata() => weekData;
+    return dataset;
+  }
+
+  List<double>? getWeekdata(BuildContext context) {
+    var args =
+        ModalRoute.of(context)!.settings.arguments as List<FinantialMovement>;
+
+    var fmList = args;
+
+    var weekdata = List.generate(fmList.length, (index) => fmList[index].value);
+
+    return weekdata;
+  }
 }
