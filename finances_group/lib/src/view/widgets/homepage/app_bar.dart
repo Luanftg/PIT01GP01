2d31714262c49f58f2b3e9a52d08b07c7048c3a1
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-11-25 13:27:02 -0300

    [test]: add registerFM page for charts tests

--- finances_group/lib/src/view/widgets/homepage/app_bar.dart
+++ finances_group/lib/src/view/widgets/homepage/app_bar.dart
@@ -18,17 +18,19 @@ const bottomAppBAr = CustomBottomAppBar(
   shape: CircularNotchedRectangle(),
 );
 
-final floatingActionButton = FloatingActionButton(
-  child: Container(
-    width: 60,
-    height: 60,
-    decoration: const BoxDecoration(
-      shape: BoxShape.circle,
-      gradient: AppCustomColors.cyanGreen,
+Widget floatingActionButton(BuildContext context) {
+  return FloatingActionButton(
+    child: Container(
+      width: 60,
+      height: 60,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: AppCustomColors.cyanGreen,
+      ),
+      child: const Icon(Icons.add),
     ),
-    child: const Icon(Icons.add),
-  ),
-  onPressed: () {
-    debugPrint('FAT');
-  },
-);
+    onPressed: () {
+      Navigator.of(context).pushNamed('/registerFM');
+    },
+  );
+}
