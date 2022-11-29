529ba2da6f1abf292a8c708febb94480f1f7a2a5
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-11-29 00:53:29 -0300

    [refact]: add paymentDate to FinantialMovement

--- finances_group/lib/src/models/finantial_movement.dart
+++ finances_group/lib/src/models/finantial_movement.dart
@@ -32,29 +32,29 @@ class FinantialMovement {
     result.addAll({'userID': userID});
     result.addAll({'isIncome': isIncome});
     result.addAll({'category': category.toMap()});
-    result.addAll({'paymentDate': paymentDate});
+    if (paymentDate != null) {
+      result.addAll({'paymentDate': paymentDate!.millisecondsSinceEpoch});
+    }
 
     return result;
   }
 
   factory FinantialMovement.fromMap(Map<String, dynamic> map) {
     return FinantialMovement(
-        id: map['id']?.toInt(),
-        description: map['description'] ?? '',
-        value: map['value']?.toDouble() ?? 0.0,
-        userID: map['userID']?.toInt() ?? 0,
-        isIncome: map['isIncome'] ?? false,
-        category: Category.fromMap(map['category']),
-        paymentDate: map['paymentDate'] ?? DateTime.now());
+      id: map['id']?.toInt(),
+      description: map['description'] ?? '',
+      value: map['value']?.toDouble() ?? 0.0,
+      userID: map['userID']?.toInt() ?? 0,
+      isIncome: map['isIncome'] ?? false,
+      category: Category.fromMap(map['category']),
+      paymentDate: map['paymentDate'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['paymentDate'])
+          : null,
+    );
   }
 
   String toJson() => json.encode(toMap());
 
   factory FinantialMovement.fromJson(String source) =>
       FinantialMovement.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'FinantialMovement(id: $id, description: $description, value: $value, userID: $userID, isIncome: $isIncome, category: $category, paymentDate: $paymentDate)';
-  }
 }
