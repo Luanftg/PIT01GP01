71a8a0848b4b21401d81b2d1b4ccc535234ed257
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-01 08:18:26 -0300

    [feat]: create function logout in LoginController

--- finances_group/lib/src/controller/login_controller.dart
+++ finances_group/lib/src/controller/login_controller.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:finances_group/src/models/user_model.dart';
 import 'package:finances_group/src/states/login_state.dart';
@@ -12,35 +11,46 @@ class LoginController {
 
     final List<String>? users = prefs.getStringList('user1');
 
-    log('Users: $users');
-
     if (users != null) {
       final decodedList = users.map((e) => jsonDecode(e)).toList();
-      log('DecodedList: $decodedList');
-      //final list = List.from(decodedList);
 
       final mapedList =
           decodedList.map((e) => UserModel.fromJson((e))).toList();
-
-      log('MapedList:$mapedList');
 
       try {
         final UserModel userFromShared = mapedList.firstWhere(
           (element) =>
               element.email == user.email && element.password == user.password,
         );
+        userFromShared.isLogged = true;
 
-        log('UserFromShared: $userFromShared');
+        final userListToSave = mapedList.map((e) => jsonEncode(e)).toList();
+        await prefs.setStringList('user1', userListToSave);
 
         state = LoginStateSucces(userFromShared);
         return state;
       } catch (e) {
-        log(e.toString());
         state = LoginStateError('Credenciais inválidas');
         return state;
       }
     }
     state = LoginStateError('Credenciais inválidas');
     return state;
+  }
+
+  void logout(UserModel userModel) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final List<String>? users = prefs.getStringList('user1');
+    final decodedList = users!.map((e) => jsonDecode(e)).toList();
+    final mapedList = decodedList.map((e) => UserModel.fromJson((e))).toList();
+    final UserModel userFromShared = mapedList.firstWhere(
+      (element) =>
+          element.email == userModel.email &&
+          element.password == userModel.password,
+    );
+    userFromShared.isLogged = false;
+    final userListToSave = mapedList.map((e) => jsonEncode(e)).toList();
+    await prefs.setStringList('user1', userListToSave);
   }
 }
