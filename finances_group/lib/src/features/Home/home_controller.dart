4f07e23fd43dc59332014d3274256063571e66d4
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-29 19:26:26 -0300

    [fix]: remove comments

--- finances_group/lib/src/features/Home/home_controller.dart
+++ finances_group/lib/src/features/Home/home_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:finances_group/src/data/repositories/repository.dart';
 
 import 'package:finances_group/src/models/finantial_movement.dart';
@@ -12,6 +14,23 @@ class HomeController extends ValueNotifier<HomeState> {
       : super(HomeStateInitial());
 
   ValueNotifier<HomeState> userLogged = ValueNotifier(HomeStateInitial());
+
+  double maxValue = 0;
+  Future<bool> create(
+      FinantialMovement finantialMovement, UserModel userModel) async {
+    var result = await _finantialMovementRepositoryLocalImp.create(
+        finantialMovement, userModel);
+    return result ? true : false;
+  }
+
+  Future<List<FinantialMovement>> findAll(UserModel userModel) async {
+    var result = await _finantialMovementRepositoryLocalImp.findAll(userModel);
+    return result;
+  }
+
+  List<FinantialMovement>? getList(UserModel args) {
+    return args.finantialMovementList;
+  }
 
   Color categoryColor(String name) {
     final Color color;
@@ -34,33 +53,4 @@ class HomeController extends ValueNotifier<HomeState> {
     }
     return color;
   }
-
-  double maxValue = 0;
-  Future<bool> create(
-      FinantialMovement finantialMovement, UserModel userModel) async {
-    var result = await _finantialMovementRepositoryLocalImp.create(
-        finantialMovement, userModel);
-    return result ? true : false;
-  }
-
-  Future<List<FinantialMovement>> findAll(UserModel userModel) async {
-    var result = await _finantialMovementRepositoryLocalImp.findAll(userModel);
-    return result;
-  }
-
-  List<FinantialMovement>? getList(UserModel args) {
-    return args.finantialMovementList;
-  }
-
-  // List<double>? getWeekdata(UserModel args) {
-  //   var fmList = args.finantialMovementList;
-
-  //   // if (fmList != null) {
-  //   //   var weekdata =
-  //   //       List.generate(fmList.length, (index) => fmList[index].value);
-  //   //   return weekdata;
-  //   // }
-  //   // args.finantialMovementList = [];
-  //   // return [];
-  // }
 }
