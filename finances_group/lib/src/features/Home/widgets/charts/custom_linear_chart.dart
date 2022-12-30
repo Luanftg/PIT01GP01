a938907ae57f902b7471ecece92e72d76f0e382a
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-30 00:47:55 -0300

    [FIX]: remove logic from initState and create a Linear chart Controller

--- finances_group/lib/src/features/Home/widgets/charts/custom_linear_chart.dart
+++ finances_group/lib/src/features/Home/widgets/charts/custom_linear_chart.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:finances_group/src/features/home/widgets/charts/liner_chart_controller.dart';
 import 'package:finances_group/src/models/finantial_movement.dart';
 import 'package:flutter/material.dart';
 
@@ -14,60 +15,29 @@ class CustomLinearChart extends StatefulWidget {
 }
 
 class _CustomLinearChartState extends State<CustomLinearChart> {
-  //late List<FinantialMovement>? weekData;
-  double minData = double.maxFinite;
-  double rangeData = 1.0;
-  double maxData = -double.maxFinite;
-  double percentage = 0.0;
-  List<double> charts = [];
-  late Timer timer;
+  double get minData => controller.minData;
+  double get rangeData => controller.rangeData;
+  double get maxData => controller.maxData;
+  double get percentage => controller.percentage;
+
+  var controller = LinearChartController();
+  List<double> get listOfDouble => controller.doubleList;
+
   @override
   void initState() {
     super.initState();
-
-    if (widget.weekData != null && widget.weekData!.isNotEmpty) {
-      for (int index = 0; index < widget.weekData!.length; index++) {
-        charts.add(widget.weekData![index].value);
+    controller.addListener(() {
+      if (mounted) {
+        setState(() {});
       }
-
-      setState(() {
-        charts = charts.take(7).toList();
-        //log('weekDataFromChart: ${weekData.toString()}');
-        for (var element in charts) {
-          minData = element < minData ? element : minData;
-          maxData = element > maxData ? element : maxData;
-        }
-        rangeData = maxData - minData;
-      });
-      if (rangeData == 0) {
-        rangeData = minData;
-        minData = 0.0;
-      }
-    } else {
-      rangeData = 0;
-      minData = 0;
-      maxData = 0;
-    }
-
-    //setup animation timer and update variables
-    const fps = 60.0;
-    const totalAnimation = 1.0;
-    var percentStep = 1.0 / (totalAnimation * fps);
-    var frameDuration = (1000 ~/ fps);
-    timer = Timer.periodic(Duration(milliseconds: frameDuration), (timer) {
-      setState(() {
-        percentage += percentStep;
-        percentage = percentage > 1.0 ? 1.0 : percentage;
-        if (percentage >= 1.0) {
-          timer.cancel();
-        }
-      });
     });
+    controller.incrementPercentage();
+    controller.getList(widget.weekData);
   }
 
   @override
   void dispose() {
-    timer.cancel();
+    controller.dispose();
     super.dispose();
   }
 
@@ -77,7 +47,7 @@ class _CustomLinearChartState extends State<CustomLinearChart> {
       margin: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
       child: CustomPaint(
         painter: CustomLinearChartPainter(
-          charts,
+          listOfDouble,
           minData,
           maxData,
           rangeData,
