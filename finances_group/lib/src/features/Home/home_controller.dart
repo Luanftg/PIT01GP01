57b8b42df5f07cc4a2bbb053b4aa8b51c5a15518
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-31 11:22:17 -0300

    [refact]: recreate homeController

--- finances_group/lib/src/features/Home/home_controller.dart
+++ finances_group/lib/src/features/Home/home_controller.dart
@@ -1,6 +1,22 @@
 import 'package:finances_group/src/features/home/home_state.dart';
 import 'package:flutter/material.dart';
 
+import '../../models/user_model.dart';
+
 class HomeController extends ValueNotifier<HomeState> {
-  HomeController(super.value);
+  HomeController() : super(HomeStateInitial());
+
+  void fetchUserLogged(UserModel userModel) {
+    try {
+      value = HomeStateLoading();
+      if (userModel.finantialMovementList != null &&
+          userModel.finantialMovementList!.isNotEmpty) {
+        value = HomeStateSucess(userModel);
+      } else {
+        value = HomeStateWelcome(userModel);
+      }
+    } catch (e) {
+      value = HomeStateError(e.toString());
+    }
+  }
 }
