32c7be147855e7d24b2a14590b262a9e498e82c6
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-08 22:11:44 -0300

    [feat]: create funtions with FirebaseFireStore

--- finances_group/lib/src/features/register_finantial_movement/finantial_movement_controller.dart
+++ finances_group/lib/src/features/register_finantial_movement/finantial_movement_controller.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:finances_group/src/data/repositories/repository.dart';
+import 'package:finances_group/src/models/category.dart';
 import 'package:finances_group/src/models/finantial_movement.dart';
 
 import '../../models/user_model.dart';
@@ -7,6 +9,26 @@ class FinantialMovementController {
   final IRepository<FinantialMovement> _repository;
 
   FinantialMovementController(this._repository);
+
+  final db = FirebaseFirestore.instance;
+  static const String categorias = 'categories';
+
+  Future<void> saveCategory(Category category) async {
+    await db.collection(categorias).add(category.toMap());
+  }
+
+  Future<List<String>> fetchCategories() async {
+    var listaDeMapDeCategoria = await db.collection(categorias).get();
+    if (listaDeMapDeCategoria.docs.isEmpty) {
+      return [];
+    }
+    List<String> listaDeCategoria = [];
+
+    listaDeMapDeCategoria.docs.forEach((element) {
+      listaDeCategoria.add(element["label"]);
+    });
+    return listaDeCategoria;
+  }
 
   Future<bool> create(
       FinantialMovement finantialMovement, UserModel userModel) async {
