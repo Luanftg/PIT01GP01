1290ec65203c011724329c0644d2c1d796e42121
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-10 17:25:37 -0300

    [refact]: change PrefService to AuthService and correct imports

--- finances_group/lib/src/data/services/prefs_service.dart
+++ finances_group/lib/src/data/services/prefs_service.dart
@@ -86,4 +86,65 @@ class PrefsService {
       await prefs.setStringList('user1', userListToSave);
     }
   }
+
+  Future<void> logar(UserModel user) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final List<String>? users = prefs.getStringList('user1');
+
+    if (users != null) {
+      final decodedList = users.map((e) => jsonDecode(e)).toList();
+
+      final mapedList =
+          decodedList.map((e) => UserModel.fromJson((e))).toList();
+      final UserModel userFromShared = mapedList.firstWhere(
+        (element) =>
+            element.email == user.email && element.password == user.password,
+      );
+      userFromShared.isLogged = true;
+
+      final userListToSave = mapedList.map((e) => jsonEncode(e)).toList();
+      await prefs.setStringList('user1', userListToSave);
+    }
+
+    void logout(UserModel userModel) async {
+      final prefs = await SharedPreferences.getInstance();
+
+      final List<String>? users = prefs.getStringList('user1');
+      final decodedList = users!.map((e) => jsonDecode(e)).toList();
+      final mapedList =
+          decodedList.map((e) => UserModel.fromJson((e))).toList();
+      final UserModel userFromShared = mapedList.firstWhere(
+        (element) =>
+            element.email == userModel.email &&
+            element.password == userModel.password,
+      );
+      userFromShared.isLogged = false;
+      final userListToSave = mapedList.map((e) => jsonEncode(e)).toList();
+      await prefs.setStringList('user1', userListToSave);
+    }
+  }
+
+  Future<bool> isAuthenticated() async {
+    final pref = await SharedPreferences.getInstance();
+    final listFromShared = pref.getStringList('user1');
+    if (listFromShared != null) {
+      final decodedList = listFromShared.map((e) => jsonDecode(e)).toList();
+      final mapedList = decodedList.map((e) => UserModel.fromJson(e)).toList();
+      final bool haveUserLogged =
+          mapedList.any((element) => element.isLogged == true);
+
+      if (haveUserLogged) {
+        final UserModel userLogged =
+            mapedList.firstWhere((element) => element.isLogged == true);
+
+        if (userLogged.isLogged != null) {
+          return userLogged.isLogged!;
+        }
+
+        return false;
+      }
+    }
+    return false;
+  }
 }
