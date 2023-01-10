bc12739917c3518d8a2dd9d824bb67aba752290f
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-10 17:28:02 -0300

    [refact]: correct imports

--- finances_group/lib/finance_app.dart
+++ finances_group/lib/finance_app.dart
@@ -1,10 +1,10 @@
+import 'package:finances_group/src/features/Splash/splash_page.dart';
 import 'package:finances_group/src/models/user_model.dart';
 import 'package:finances_group/src/shared/design/colors/app_custom_colors.dart';
 import 'package:finances_group/src/shared/design/theme/custom_theme_data.dart';
 import 'package:finances_group/src/features/home/home_page.dart';
 import 'package:finances_group/src/features/login/login_page.dart';
 import 'package:finances_group/src/features/register/register_page.dart';
-import 'package:finances_group/src/features/splash/splash_page.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
