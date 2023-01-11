12a9a6f28ef3adc6bfe23b6f8b5ba90d938acc81
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-11 13:14:29 -0300

    [refact]: correct imports

--- finances_group/lib/src/features/Register/register_page.dart
+++ finances_group/lib/src/features/Register/register_page.dart
@@ -1,4 +1,3 @@
-import 'package:finances_group/src/features/Register/register_controller.dart';
 import 'package:finances_group/src/shared/design/colors/app_custom_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
