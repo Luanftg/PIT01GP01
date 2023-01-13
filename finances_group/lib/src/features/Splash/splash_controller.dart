cbfd26c25927907ce8eac35a548c1beccdc75a82
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-13 13:54:00 -0300

    [REFACT]: now isAuthenticated disdnt return a state  and remove Future.delayed from page

--- finances_group/lib/src/features/Splash/splash_controller.dart
+++ finances_group/lib/src/features/Splash/splash_controller.dart
@@ -1,19 +1,11 @@
 import 'package:finances_group/src/data/services/auth.service.dart';
 import 'package:finances_group/src/models/user_model.dart';
 
-import 'splash_state.dart';
-
 class SplashController {
-  SplashController();
-
-  Future<SplashState> isAuthenticated() async {
-    AuthService authService = AuthService();
-
+  AuthService authService = AuthService();
+  Future<UserModel?> isAuthenticated() async {
+    Future.delayed(const Duration(seconds: 2));
     final UserModel? userLogged = await authService.isUserLoged();
-
-    if (userLogged != null) {
-      return UserLogedSplashState(userLogged);
-    }
-    return UserUnLogedSplashState();
+    return userLogged;
   }
 }
