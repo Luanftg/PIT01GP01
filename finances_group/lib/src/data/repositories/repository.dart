c50b47321365e2f7d53ba071d3d92570c6aedda9
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-13 13:33:52 -0300

    [REFACT]: remove UserModel dependency on Repository

--- finances_group/lib/src/data/repositories/repository.dart
+++ finances_group/lib/src/data/repositories/repository.dart
@@ -1,9 +1,7 @@
-import '../../models/user_model.dart';
-
 abstract class IRepository<T> {
-  Future<bool> create(T value, UserModel userModel);
-  Future<T?> findOne(String id, UserModel userModel);
-  Future<List<T>> findAll(UserModel userModel);
-  Future<bool> delete(String id, UserModel userModel);
-  Future<bool> update(T value, UserModel userModel);
+  Future<bool> create(T value);
+  Future<T?> findOne(String id);
+  Future<List<T>> findAll();
+  Future<bool> delete(String id);
+  Future<bool> update(T value);
 }
