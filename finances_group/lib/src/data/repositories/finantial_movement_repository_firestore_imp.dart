b6d2bc4730aeec99cc577151375b89e2e6f372bc
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-13 13:33:09 -0300

    [REFACT]: CRUD FinantialMovement

--- finances_group/lib/src/data/repositories/finantial_movement_repository_firestore_imp.dart
+++ finances_group/lib/src/data/repositories/finantial_movement_repository_firestore_imp.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:finances_group/src/data/repositories/repository.dart';
 
 import 'package:finances_group/src/models/finantial_movement.dart';
-import 'package:finances_group/src/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FinantialMovementRepositoryFirestoreImp
     implements IRepository<FinantialMovement> {
@@ -12,25 +12,42 @@ class FinantialMovementRepositoryFirestoreImp
   static const String users = 'users';
 
   static final db = FirebaseFirestore.instance;
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  String userId = '';
+
+  String get _getUserId {
+    userId = firebaseAuth.currentUser?.uid ?? '';
+    return userId;
+  }
 
   @override
-  Future<bool> create(FinantialMovement value, UserModel userModel) async {
-    if (userModel.id != null) {
+  Future<bool> create(FinantialMovement value) async {
+    if (_getUserId.isNotEmpty) {
       try {
-        var docRef = await db
-            .collection(users)
-            .doc(userModel.id!)
-            .collection(finantialMovement)
-            .add(value.toMap());
-        var fmWithId = await docRef.get();
+        var colectionReference =
+            db.collection(users).doc(_getUserId).collection(finantialMovement);
+
+        bool fmExist;
+        var docRef = await colectionReference.get();
+        if (docRef.docs.isEmpty) {
+          fmExist = false;
+        } else {
+          fmExist =
+              docRef.docs.every((element) => element.data()["id"] == value.id);
+        }
+
+        if (fmExist) {
+          //EDITA
+          var result = await update(value);
+          return result ? true : false;
+        }
+
+        //Salva
+        var newDocRef = await colectionReference.add(value.toMap());
+        var fmWithId = await newDocRef.get();
         value.id = fmWithId.id;
-        await db
-            .collection(users)
-            .doc(userModel.id)
-            .collection(finantialMovement)
-            .doc(fmWithId.id)
-            .set(value.toMap());
-        return true;
+        var result = await update(value);
+        return result ? true : false;
       } catch (e) {
         throw Exception(e.toString());
       }
@@ -39,30 +56,34 @@ class FinantialMovementRepositoryFirestoreImp
   }
 
   @override
-  Future<bool> delete(String id, UserModel userModel) async {
+  Future<bool> delete(String id) async {
     try {
       var docRef = db
           .collection(users)
-          .doc(userModel.id)
+          .doc(_getUserId)
           .collection(finantialMovement)
           .doc(id);
       var doc = await docRef.get();
-      log('-------------$doc-------');
-      if (doc.exists) await docRef.delete();
 
-      return true;
+      var result = doc.exists;
+      if (result) {
+        await docRef.delete();
+      }
+
+      return result;
     } catch (e) {
       throw Exception("[ERRO no DELETE do FireBase] -> ${e.toString()}");
     }
   }
 
   @override
-  Future<List<FinantialMovement>> findAll(UserModel userModel) async {
+  Future<List<FinantialMovement>> findAll() async {
     var mapedList = await db
         .collection(users)
-        .doc(userModel.id)
+        .doc(_getUserId)
         .collection(finantialMovement)
         .get();
+
     if (mapedList.docs.isEmpty) {
       return [];
     }
@@ -73,41 +94,36 @@ class FinantialMovementRepositoryFirestoreImp
   }
 
   @override
-  Future<FinantialMovement?> findOne(String id, UserModel userModel) async {
-    if (userModel.finantialMovementList != null &&
-        userModel.finantialMovementList!.isNotEmpty) {
-      try {
-        var docSnap = await db
-            .collection(users)
-            .doc(userModel.id)
-            .collection(finantialMovement)
-            .doc(id)
-            .get();
-        if (docSnap.exists) {
-          var fmMap = docSnap.data();
-          var fm = FinantialMovement.fromMap(fmMap!);
-          return fm;
-        }
-        return null;
-      } catch (e) {
-        throw Exception('[Erro no findONE do FireBase] -> ${e.toString()}');
+  Future<FinantialMovement?> findOne(String id) async {
+    try {
+      var docSnap = await db
+          .collection(users)
+          .doc(_getUserId)
+          .collection(finantialMovement)
+          .doc(id)
+          .get();
+      if (docSnap.exists) {
+        var fmMap = docSnap.data();
+        var fm = FinantialMovement.fromMap(fmMap!);
+        return fm;
       }
-    } else {
       return null;
+    } catch (e) {
+      throw Exception('[Erro no findONE do FireBase] -> ${e.toString()}');
     }
   }
 
   @override
-  Future<bool> update(FinantialMovement value, UserModel userModel) async {
+  Future<bool> update(FinantialMovement value) async {
     if (value.id == null) {
       return false;
     }
     try {
       await db
           .collection(users)
-          .doc(userModel.id)
+          .doc(_getUserId)
           .collection(finantialMovement)
-          .doc(userModel.id)
+          .doc(value.id)
           .set(value.toMap());
       return true;
     } catch (e) {
