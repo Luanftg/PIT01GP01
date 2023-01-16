ea79b4830874aa2353b7397c7196fb3c1eba409f
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-16 14:04:58 -0300

    [REFACT]: authServeice from InjectionDependency

--- finances_group/lib/src/features/Splash/splash_controller.dart
+++ finances_group/lib/src/features/Splash/splash_controller.dart
@@ -2,7 +2,11 @@ import 'package:finances_group/src/data/services/auth.service.dart';
 import 'package:finances_group/src/models/user_model.dart';
 
 class SplashController {
-  AuthService authService = AuthService();
+  AuthService authService;
+  SplashController({
+    required this.authService,
+  });
+
   Future<UserModel?> isAuthenticated() async {
     Future.delayed(const Duration(seconds: 2));
     final UserModel? userLogged = await authService.isUserLoged();
