a44a258d72fda393cc93d694c2633e46c9025708
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-17 01:44:11 -0300

    [refact]: remove log

--- finances_group/lib/src/data/services/firestore_service.dart
+++ finances_group/lib/src/data/services/firestore_service.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:finances_group/src/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
