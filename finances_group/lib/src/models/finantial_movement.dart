b0d350bde231d612b5c10255a2289e3f73fb5e27
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-18 14:12:03 -0300

    [REFACT]: now UserModel depends of Uuid

--- finances_group/lib/src/models/finantial_movement.dart
+++ finances_group/lib/src/models/finantial_movement.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 
 import 'package:finances_group/src/models/category.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
 
 class FinantialMovement {
-  String? id;
+  late String id;
   final String description;
   final double value;
   final String userID;
@@ -11,22 +14,23 @@ class FinantialMovement {
   final Category category;
   final String? paymentDate;
 
-  FinantialMovement({
-    this.id,
+  FinantialMovement(
+    this.id, {
     required this.description,
     required this.value,
     required this.userID,
     required this.isIncome,
     required this.category,
     this.paymentDate,
-  });
+  }) {
+    if (id == '') {
+      id = uuid.v1();
+    }
+  }
 
   Map<String, dynamic> toMap() {
     final result = <String, dynamic>{};
-
-    if (id != null) {
-      result.addAll({'id': id});
-    }
+    result.addAll({'id': id});
     result.addAll({'description': description});
     result.addAll({'value': value});
     result.addAll({'userID': userID});
@@ -41,7 +45,7 @@ class FinantialMovement {
 
   factory FinantialMovement.fromMap(Map<String, dynamic> map) {
     return FinantialMovement(
-      id: map['id'] ?? '',
+      map['id'],
       description: map['description'] ?? '',
       value: map['value']?.toDouble() ?? 0.0,
       userID: map['userID'] ?? '',
