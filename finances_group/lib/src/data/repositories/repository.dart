56b63b5876b707556a7c9eed64ca9f35b600321c
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-19 13:53:28 -0300

    [refact]: findAll now receive a userID

--- finances_group/lib/src/data/repositories/repository.dart
+++ finances_group/lib/src/data/repositories/repository.dart
@@ -1,7 +1,7 @@
 abstract class IRepository<T> {
   Future<void> create({required T value});
   Future<T?> findOne(String id);
-  Future<List<T>> findAll();
+  Future<List<T>> findAll(String userID);
   Future<void> delete(String id);
   Future<void> update(T value);
 }
