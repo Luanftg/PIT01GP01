b54bdeaf6da4d9933378eeaebd0ff4a808f05203
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-19 14:00:12 -0300

    [test]: write test

---
+++ finances_group/test/src/data/repositories/firebase_auth_repository_test.dart
@@ -0,0 +1,34 @@
+// import 'package:flutter_test/flutter_test.dart';
+
+
+// void main() {
+//   setUp(() {
+    // final db = FirebaseFirestore.instance;
+    // final FirebaseAuth _auth = FirebaseAuth.instance;
+    // const String users = 'users';
+//   });
+//   group('[Method]: Future<UserModel?> login(String email, String password)',
+//       () {
+//     test('should return a [UserModel] from firebase', () {});
+//     test(
+//         'should return a [AuthException] when catch FirebaseAuthException with code [weak-password] from firebase',
+//         () {});
+//     test(
+//         'should return a [AuthException] when catch FirebaseAuthException with code [email-already-in-use] from firebase',
+//         () {});
+//   });
+
+//   group('[Method]: void register(UserModel userModel)', () {
+//     test('should register a single [UserModel] on firebase', () {});
+//   });
+
+//   group('[Method]: Future<UserModel?> isUserLoged()', () {
+//     test(
+//         '[FirebaseAuth.instance.currentUser] should return a [UserModel] from firebase',
+//         () {});
+//   });
+
+  // group('[Method]: logout()', () {
+  //   test('[FirebaseAuth.instance.currentUser] should return null', () {});
+  // });
+// }
