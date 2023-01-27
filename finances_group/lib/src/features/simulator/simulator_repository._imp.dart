81d1c5225605592dc3433b4311b23c990716ac3e
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-27 19:41:34 -0300

    [refact]: now use a collection on FireBase to save only a simulator

--- finances_group/lib/src/features/simulator/simulator_repository._imp.dart
+++ finances_group/lib/src/features/simulator/simulator_repository._imp.dart
@@ -9,8 +9,6 @@ class SimulatorRepositoryImp implements SimulatorRepository {
   @override
   Future<List<ValorSimulado>> getList(String userId) async {
     final result = await storage
-        .collection('users')
-        .doc(userId)
         .collection('simulator')
         .where('userId', isEqualTo: userId)
         .get();
@@ -24,8 +22,6 @@ class SimulatorRepositoryImp implements SimulatorRepository {
   @override
   Future<void> addOrUpdateSimulatorValue(ValorSimulado valorSimulado) async {
     final result = await storage
-        .collection('users')
-        .doc(valorSimulado.userId)
         .collection('simulator')
         .where('userId', isEqualTo: valorSimulado.userId)
         .get();
@@ -33,17 +29,11 @@ class SimulatorRepositoryImp implements SimulatorRepository {
     if (result.docs.isNotEmpty) {
       final docId = result.docs.first.id;
       await storage
-          .collection('users')
-          .doc(valorSimulado.userId)
           .collection('simulator')
           .doc(docId)
           .set(valorSimulado.toMap());
     } else {
-      await storage
-          .collection('users')
-          .doc(valorSimulado.userId)
-          .collection('simulator')
-          .add(valorSimulado.toMap());
+      await storage.collection('simulator').add(valorSimulado.toMap());
     }
   }
 }
