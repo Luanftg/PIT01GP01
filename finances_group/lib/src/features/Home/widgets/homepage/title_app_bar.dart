4a41d23c8605abdddda620e48432020253dba5c9
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-28 11:42:24 -0300

    [refact]: titleAppbar now receive a usermodel and fix circle avatar

--- finances_group/lib/src/features/Home/widgets/homepage/title_app_bar.dart
+++ finances_group/lib/src/features/Home/widgets/homepage/title_app_bar.dart
@@ -1,37 +1,52 @@
+import 'dart:io';
+
+import 'package:finances_group/src/models/user_model.dart';
 import 'package:finances_group/src/shared/design/theme/text_theme.dart';
 
 import 'package:flutter/material.dart';
 
 class TitleAppBar extends StatelessWidget {
-  final String? userName;
-  final String? userImage;
+  final UserModel userModel;
   const TitleAppBar({
     Key? key,
-    required this.userName,
-    required this.userImage,
+    required this.userModel,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Row(
       children: [
         Padding(
           padding: const EdgeInsets.only(right: 8),
           child: GestureDetector(
-              onTap: () => {Scaffold.of(context).openDrawer()},
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  userImage?.isNotEmpty ?? false
-                      ? userImage ?? ''
-                      : 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg',
-                ),
-                radius: 24,
-              )),
+            onTap: () => {Scaffold.of(context).openDrawer()},
+            child: CircleAvatar(
+              radius: 24,
+              child: ClipOval(
+                child: userModel.photoURL?.isEmpty ?? true
+                    ? Image.network(
+                        "https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg",
+                        fit: BoxFit.fitWidth,
+                        width: width * 0.4,
+                      )
+                    : Image.file(
+                        File(userModel.photoURL ?? ''),
+                        fit: BoxFit.fitWidth,
+                        width: width * 0.4,
+                      ),
+              ),
+            ),
+          ),
         ),
         Text(
-          userName ?? 'Usuário sem nome',
+          userModel.name,
+          softWrap: true,
+          overflow: TextOverflow.clip,
+          maxLines: 2,
+          textHeightBehavior: const TextHeightBehavior(),
           style: CustomAppTextTheme.body.copyWith(
-            fontSize: 18,
+            fontSize: 16,
           ),
         ),
       ],
