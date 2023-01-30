bf80dbd25dac7fedd992096db375056dfc396a30
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-30 10:04:43 -0300

    [feat]: donut Chart now render per category and use bool parameters isObscure

--- finances_group/lib/src/features/Home/widgets/charts/dounut_chart_controller.dart
+++ finances_group/lib/src/features/Home/widgets/charts/dounut_chart_controller.dart
@@ -4,10 +4,14 @@ import 'package:flutter/cupertino.dart';
 
 class DonutChartController extends ChangeNotifier {
   double fullAngle = 0;
-
+  static ValueNotifier<bool> isObscure = ValueNotifier(false);
   double secondsToComplete = 5;
 
   late Timer timer;
+
+  static void changeIsObscure() {
+    isObscure.value = !isObscure.value;
+  }
 
   void incrementFullAngle() {
     Timer.periodic(const Duration(milliseconds: 1000 ~/ 60), (timer) {
