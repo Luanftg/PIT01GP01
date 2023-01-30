c2d777f2230bbb33f469b4a8116491a040bc656c
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-30 19:05:19 -0300

    [feat]: create function that return a string month from an int

---
+++ finances_group/lib/src/shared/utils/month.dart
@@ -0,0 +1,33 @@
+class Month {
+  static String returnLabel(int month) {
+    switch (month) {
+      case 1:
+        return 'Janeiro';
+      case 2:
+        return 'Fevereiro';
+      case 3:
+        return 'Março';
+      case 4:
+        return 'Abril';
+      case 5:
+        return 'Maio';
+      case 6:
+        return 'Junho';
+      case 7:
+        return 'Julho';
+      case 8:
+        return 'Agosto';
+      case 9:
+        return 'Setembro';
+      case 10:
+        return 'Outubro';
+      case 11:
+        return 'Novembro';
+      case 12:
+        return 'Dezembro';
+
+      default:
+    }
+    return '';
+  }
+}
